114a8b8ad46ab6a0fa9b8a0cb26c6a44c15eb8a7
Raffian Moin <raffianmoin69@gmail.com>
2026-02-17 19:46:00 +0600

    Add ability to connect multiple clients to server with persistence

--- client.c
+++ client.c
@@ -2,27 +2,88 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include<netdb.h>
+#include<string.h>
+#include<fcntl.h>
 
 #define SERVER_IP_ADDRESS "127.0.0.1" 
+#define SERVER_PORT "3009" 
 
 int main() {
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    inet_aton(SERVER_IP_ADDRESS, &(server_addr.sin_addr));
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
 
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    int get_addr_return_val;
+    int socket_fd;
+    struct addrinfo *p;
 
-    if (socket_fd == -1) {
-        perror("Socket creation failed");
+    if ((get_addr_return_val = getaddrinfo("localhost", "3009", &hints, &res)) != 0) {
+        printf("%s\n", gai_strerror(get_addr_return_val));
     }
 
-    if (connect(socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
-        perror("Failed connect to server");
+    for(p = res; p != NULL; p = p->ai_next) {
+        if ((socket_fd = socket(p->ai_family, p->ai_socktype, 0)) == -1) {
+            perror("socket\n");
+            continue;
+        }
+
+        if (connect(socket_fd, p->ai_addr, p->ai_addrlen) == -1) {
+            perror("connect\n");
+            continue;
+        }
+
+        break;
+
     }
 
-    char buff[] = "Hello form client!";
-    send(socket_fd, buff, sizeof(buff), 0);
+    if (p == NULL) {
+        printf("attempt to connect to server is failed\n");
+        return 1;
+    }
+    
+    freeaddrinfo(res);
 
-    close(socket_fd);
+    printf("connection successful\n");
+
+    int fds[2];
+    fd_set readfds;
+    fd_set writefds;
+    struct timeval timeout = {2, 0};
+    int max_fd = socket_fd;
+
+    while(1) { 
+        
+        FD_ZERO(&readfds);
+
+        FD_SET(STDIN_FILENO, &readfds);
+        FD_SET(socket_fd, &readfds);
+        
+
+        int readyfds = select(max_fd + 1, &readfds, NULL, NULL, &timeout);
+        
+        char out[50];
+        if (readyfds > 0) {
+            if (FD_ISSET(STDIN_FILENO, &readfds)) {
+                if(fgets(out, sizeof(out), stdin) != NULL) {
+                    if (send(socket_fd, out, strlen(out), 0) == -1) {
+                        printf("failed to send message to client\n");
+                    } 
+                    memset(out, 0, sizeof(out));
+                }
+            }
+
+            if (FD_ISSET(socket_fd, &readfds)) {
+                char buff[50];
+                memset(buff, 0, sizeof(buff));
+
+                int recvint = recv(socket_fd, buff, sizeof(buff), 0);
+                if (recvint > 0) {
+                    printf("received: %s\n", buff);
+                }
+            }
+        }
+
+    }
 }
--- server.c
+++ server.c
@@ -1,43 +1,147 @@
 #include<stdio.h>
+#include<sys/types.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include<netdb.h>
+#include<string.h>
+#include<fcntl.h>
+#include<errno.h>
 
 #define SERVER_IP_ADDRESS "127.0.0.1"
 
+void *get_in_address(struct sockaddr *peer_addr) {
+        if (peer_addr->sa_family == AF_INET) {
+            return &(((struct sockaddr_in *)&peer_addr)->sin_addr);
+        } else { 
+            return &(((struct sockaddr_in6 *)&peer_addr)->sin6_addr);
+        }
+}
+
+struct fdstruct {
+    int capacity;
+    int total;
+    int fds[10];
+};
+
 int main() {
-
-    struct sockaddr_in server_addr, peer_addr;
-    int peer_size = sizeof(peer_size);
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    inet_aton(SERVER_IP_ADDRESS, &(server_addr.sin_addr));
-
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (socket_fd == -1) {
-        perror("Socket creation failed");
-    }
-
-    if (bind(socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
-        perror("Socket binding failed");
-    }
-
-    if (listen(socket_fd, SOMAXCONN) == -1) {
-        perror("Failed listening to the socket");
-    }
     
-    int connection_fd = accept(socket_fd, (struct sockaddr *) &peer_addr, &peer_size);
-    if (connection_fd == -1) {
-        perror("Failed to accept the socket");
-    }
+    struct addrinfo hints, *res;
     
-    char buff[1048];
-    recv(connection_fd, buff, sizeof(buff), 0);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    int get_addr_return_val;
+    int yes = 1;
+    int socket_fd;
+    
+    struct sockaddr_storage peer_addr;
+    socklen_t peer_addr_size;
+    peer_addr_size = sizeof(peer_addr);
+    char peer_addr_text[INET6_ADDRSTRLEN];
 
-    printf("%s\n", buff);
+     
+    if((get_addr_return_val = getaddrinfo(NULL, "3009", &hints, &res)) != 0) {
+        printf("%s\n", gai_strerror(get_addr_return_val));
+    }
 
-    close(socket_fd);
+    for(struct addrinfo *p = res; p != NULL; p = p->ai_next) {
+        if ((socket_fd = socket(p->ai_family, p->ai_socktype, 0)) == -1) {
+            perror("socket\n");
+            continue;
+        }
+            
+        int flags = fcntl(socket_fd, F_GETFL);
+        flags |= O_NONBLOCK; 
+        fcntl(socket_fd, F_SETFL, flags);
+        
+        if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+            perror("Socket option error\n");
+        }
 
+        printf("socket fd: %d\n", socket_fd);
+        
+        if (bind(socket_fd, p->ai_addr, p->ai_addrlen) == -1) {
+            perror("bind\n");
+            continue;
+        }
+
+        break;
+
+    }
+
+    freeaddrinfo(res);
+    
+    if (listen(socket_fd, 5) == -1) {
+        perror("listen failed\n");
+    }
+
+    struct fdstruct fdstr;
+    fdstr.capacity = 10;
+    fdstr.total = 0;
+    fd_set readfds;
+    struct timeval timeout = {2, 0};
+    int max_fd = socket_fd;
+
+    fdstr.fds[fdstr.total] = socket_fd;
+    fdstr.total++;
+
+    while(1) {
+        FD_ZERO(&readfds);
+
+        for(int i = 0; i < fdstr.total; i++) {
+            FD_SET(fdstr.fds[i], &readfds);
+            if (fdstr.fds[i] > max_fd) {
+                max_fd = fdstr.fds[i];
+            }
+        }
+
+        int readyfds = select(max_fd + 1, &readfds, NULL, NULL, &timeout);
+        if (readyfds > 0) {
+            printf("%d\n", fdstr.total);
+            for(int i = 0; i < fdstr.total; i++) {
+                printf("ok\n");
+                if(FD_ISSET(fdstr.fds[i], &readfds)) {   
+                    printf("inside read\n");
+                    char buff[50];
+                    memset(buff, 0, sizeof(buff));
+
+                    int recvint = recv(fdstr.fds[i], buff, sizeof(buff), 0);
+                    if (recvint > 0) {
+                        printf("received text: %s", buff);
+                        for(int j = 0; j < fdstr.total; j++) {
+                            if (fdstr.fds[j] != socket_fd) {
+                                if (send(fdstr.fds[j], buff, strlen(buff), 0) == -1) {
+                                    printf("failed to send message to client");
+                                } 
+                            }
+                        }
+                    }
+
+                }
+            }
+            
+
+            if(FD_ISSET(socket_fd, &readfds)) {   
+                int accept_fd;
+                if ((accept_fd = accept(socket_fd, (struct sockaddr *)&peer_addr, &peer_addr_size)) == -1) { 
+                    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                        printf("hmm\n");
+                        continue;
+                    }
+
+                    perror("accept failed\n");
+                    continue;
+                }
+
+                fdstr.fds[fdstr.total] = accept_fd;
+                fdstr.total++;
+                inet_ntop(peer_addr.ss_family, get_in_address((struct sockaddr *)&peer_addr), peer_addr_text, peer_addr_size);
+                
+                printf("%s connected to the server\n", peer_addr_text);
+            }
+
+        }
+    }
 }
